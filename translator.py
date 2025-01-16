fc039b8de45b9ce9f796b5cd9177efec6722801f
chenphxx <john21950@outlook.com>
2025-01-16 17:37:55 +0800

    V2025.1.16

--- translator.py
+++ translator.py
@@ -5,11 +5,11 @@ import re
 
 
 # 账号信息
-endpoint = "cn-north-4"
-huawei_account = "c3300621609"
-IAM_user = "IAM_user1"
-password = "t$YeDMKJPINw8abph0OSUNI8QF%$C"
-project = "bcb1cfc851be4b59917d52b1894d513f"
+endpoint = "your_endpoint"
+huawei_account = "your_account"
+IAM_user = "your_username"
+password = "your_password"
+project = "your_project_id"
 
 # 获取IAM Token
 def get_iam_token():
